f7c0c867b12d0ba0dd9ddb79c967a0bf71344dfc
Gleibson Pereira <gleibson.santos05@gmail.com>
2021-07-26 19:49:13 -0300

    proximo comit

--- java/script.js
+++ java/script.js
@@ -12,7 +12,7 @@ function updateClock(){
 
     digitalElement.innerHTML = `${fixZero(hour)}:${fixZero(minute)}:${fixZero(second)}`;
 
-    let sDeg = (360 / 60) * second;
+    let sDeg = ((360 / 60) * second) - 90;
 
     sElement.style.transform = `rotate(${sDeg}deg)`;
 }
